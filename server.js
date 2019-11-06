35c0fe15268f5bcf1e9b7f924ae7cbe0d45e5b15
maryamhdr <maryamhdr76@gmail.com>
2019-11-06 14:10:56 +0330

    create simple http server

--- server.js
+++ server.js
@@ -2,3 +2,37 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
+http.createServer(function (req, res) {
+
+    var pathname = url.parse(req.url, true).pathname;
+    var ext = pathname.substr(pathname.indexOf('.') + 1, pathname.length);
+    var address = req.url.substr(1, req.url.length);
+    ext = converExt(ext);
+
+    fs.readFile(address, function (err, data) {
+        if (err) {
+            res.writeHead(500);
+            res.write(err.name);
+            res.end();
+        }
+        res.writeHead(200, {
+            'Content-Type': `text/${ext}`
+        });
+        res.write(data);
+        res.end();
+    });
+
+}).listen(8080);
+
+function converExt(ext) {
+    switch (ext) {
+        case 'html':
+        case 'css':
+            ext;
+            break;
+        case 'js':
+            ext = 'javascript';
+            break;
+    }
+    return ext;
+}
